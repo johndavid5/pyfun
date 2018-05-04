f5637f74e522a11644ad49108d8bbb3f171f8e0a
John D. Aynedjian <johndavid5@yahoo.com>
2018-05-04 18:54:43 -0400

    First working version of tcalc...

--- tcalc.py
+++ tcalc.py
@@ -1,9 +1,11 @@
 import sys
 import re
+import json
 import logging
 import logging.config
 
-DEBUG = True
+DEBUG = False
+VIA_COMMAND_LINE = False
 
 #logging.config.fileConfig('logging.conf')
 # create logger
@@ -15,6 +17,32 @@ DEBUG = True
 #logging.basicConfig(filename='tcalc.log',level=logging.DEBUG)
 
 def main():
+
+    s_line = ""
+    global DEBUG
+    global VIA_COMMAND_LINE
+
+    for i in range(len(sys.argv)):
+        debug_print("sys.argv[%d]=\"%s\""%(i, sys.argv[i]))
+        if sys.argv[i].lower() == "-dbg":
+            i += 1
+            i_debug = int(sys.argv[i])
+            if( i_debug ):
+                DEBUG = True
+            else:
+                DEBUG = False
+        elif sys.argv[i].lower() == "-c":
+            VIA_COMMAND_LINE=True
+            i += 1
+            s_line = sys.argv[i]
+            print("+ " + s_line + " = " + do_line(s_line));
+
+    print("DEBUG="+str(DEBUG))
+    print("VIA_COMMAND_LINE="+str(VIA_COMMAND_LINE))
+
+    if VIA_COMMAND_LINE:
+        exit(0)
+
     # terminal-based hours:minutes::seconds time add / subtract calculator
     while 1:
         #sys.stdout.write("tcalc>");
@@ -39,46 +67,71 @@ def main():
 RE_LINE = re.compile("^(\\d+):(\\d+):(\\d+)\\s*(\\+|\\-|to)\\s*(\\d+):(\\d+):(\\d+)$", re.IGNORECASE)
 
 def do_line(s_line):
+
     sWho = "do_line" 
+    s_out = ""
     debug_print(sWho + "(): s_line = \"" + s_line + "\"...");
+
     match = RE_LINE.match(s_line) 
+
     if( match ):
         debug_print(sWho + "(): SHEMP: match.group(0) = \"" + match.group(0) + "\"...")
-        num_groups = len(match.groups())
-        groups = match.groups()
+        arr_groups = match.groups()
+        num_groups = len(arr_groups)
         debug_print(sWho + "(): SHEMP: num_groups = " + str(num_groups) + "...")
-        debug_print(sWho + "(): SHEMP: groups = \"" + list_to_short_string(groups) + "\"...")
-        debug_print(sWho + "(): SHEMP: groups = \n" + list_to_string(groups, "groups","  ") )
+        #debug_print(sWho + "(): SHEMP: groups = \"" + list_to_short_string(groups) + "\"...")
+        debug_print(sWho + "(): SHEMP: arr_groups = \"" + json.dumps(arr_groups) + "\"...")
+        debug_print(sWho + "(): SHEMP: \n" + list_to_string(arr_groups, "arr_groups","  ") )
 
         for i in range(0,num_groups+1):
             debug_print(sWho + "(): SHEMP: match.group(" + str(i) + ") = " + match.group(i) + "...")
 
-        hours_a = match.group(1)
-        minutes_a = match.group(2)
-        seconds_a = match.group(3)
+        map_a = {}
+        map_a['hours'] = int(match.group(1))
+        map_a['minutes'] = int(match.group(2))
+        map_a['seconds'] = int(match.group(3))
 
-        operator = match.groups(4)
+        debug_print(sWho + "(): SHEMP: Moe, map_a = %s..." % json.dumps(map_a) )
 
-        tot_seconds_a = hms_to_s(hours_a, minutes_a, seconds_a)
+        s_operator = match.group(4)
+        debug_print(sWho + "(): SHEMP: Moe, s_operator = \"%s\"..." % s_operator )
+        #debug_print(sWho + "(): SHEMP: Moe, s_operator = \"" + s_operator + "\"...")
 
-        hours_b = match.group(5)
-        minutes_b = match.group(6)
-        seconds_b = match.group(7)
+        tot_seconds_a = hms_to_s(map_a['hours'], map_a['minutes'], map_a['seconds'])
+        debug_print(sWho + "(): SHEMP: Moe, tot_seconds_a = %d..." % tot_seconds_a )
 
-        tot_seconds_b = hms_to_s(hours_b, minutes_b, seconds_b)
+        map_b = {}
+        map_b['hours'] = int(match.group(5))
+        map_b['minutes'] = int(match.group(6))
+        map_b['seconds'] = int(match.group(7))
 
-        tot_seconds_op = None
+        debug_print(sWho + "(): SHEMP: Moe, map_b = %s..." % json.dumps(map_b) )
 
-        if( operator == "-" ):
-            tot_seconds_op = tot_seconds_a - tot_seconds_b
+        tot_seconds_b = hms_to_s(map_b['hours'], map_b['minutes'], map_b['seconds'])
+        debug_print(sWho + "(): SHEMP: Moe, tot_seconds_b = %d..." % tot_seconds_b )
 
-        [hours_op,minutes_op,seconds_op] = s_to_hms(tot_seconds_op)  
+        tot_seconds_c = None
+
+        if( s_operator == "-" ):
+            tot_seconds_c = tot_seconds_a - tot_seconds_b
+        elif( s_operator == "+" ):
+            tot_seconds_c = tot_seconds_a + tot_seconds_b
+        elif( s_operator == "to" ):
+            tot_seconds_c = tot_seconds_b - tot_seconds_a
+
+        debug_print(sWho + "(): SHEMP: Moe, tot_seconds_c = %d..." % tot_seconds_c )
+
+        out_map = s_to_hms(tot_seconds_c) 
+        debug_print(sWho + "(): SHEMP: Moe, out_map = " + json.dumps(out_map) + "...")
         
+        #s_out = "%d:%02d:%02d" % str(out_map['hours']) + ":" + str(out_map['minutes']) + ":" + str(out_map['seconds'])
+        s_out = "%d:%02d:%02d" % (out_map['hours'], out_map['minutes'], out_map['seconds'] )
 
     else:
         debug_print(sWho + "(): SHEMP: Sorry, Moe, no match...");
+        s_out = "???"
     
-    return "???"
+    return s_out
 
 
 def list_to_string(list,name="list", prefix=""):
@@ -101,10 +154,25 @@ def list_to_short_string(list):
         s_out += str(list[i])
     return s_out
 
+
+SECONDS_PER_HOUR = 3600
+SECONDS_PER_MINUTE = 60
+
+def hms_to_s(hours, minutes, seconds):
+    return hours*SECONDS_PER_HOUR + minutes*SECONDS_PER_MINUTE + seconds
+
 def debug_print(msg):
     if DEBUG:
         print("*** " + msg )
 
+def s_to_hms(total_seconds): 
+    out_map = {}
+    out_map['hours'] = int( total_seconds / SECONDS_PER_HOUR )
+    total_seconds = total_seconds % SECONDS_PER_HOUR
+    out_map['minutes'] = int( total_seconds / SECONDS_PER_MINUTE )
+    total_seconds = total_seconds % SECONDS_PER_MINUTE
+    out_map['seconds'] = total_seconds
+    return out_map
 
 # Automatically kick-start main()...
 # https://stackoverflow.com/questions/1590608/is-it-possible-to-forward-declare-a-function-in-python
