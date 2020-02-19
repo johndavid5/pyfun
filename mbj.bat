5f46434498f054c2c50b64ff1d62253ca54aea47
John D. Aynedjian <johndavid5@yahoo.com>
2020-02-19 18:04:52 -0500

    Update mbj.bat

--- mbj.bat
+++ mbj.bat
@@ -1,2 +1,3 @@
 REM py metabolic-rate.py -help
-py metabolic-rate.py -mf "m" -wlb "245.0" -hin "70.5" -ayr "53" 2>&1 | tee p.out
+REM py metabolic-rate.py -mf "m" -wlb "245.0" -hin "70.5" -ayr "53" 2>&1 | tee p.out
+py metabolic-rate.py -mf "m" -wlb "225.0" -hin "70.5" -ayr "55" 2>&1 | tee p.out
